049c5807d7ce0e3f489a09bd5630aca4bb9c59c6
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-09-30 15:43:35 +0900

    不要な処理削除

--- post_slack_sample/main.py
+++ post_slack_sample/main.py
@@ -2,30 +2,31 @@ import application_setting.my_logger as mylogger
 from logging import Logger
 import application_setting.my_credential as mycredential
 from slack_sdk import WebClient
+import textwrap
 
 # loggerを取得
 logger: Logger = mylogger.get_logger("main")
 
 
 def main():
-    logger.info("hello, world.")
     # Slack WebClientの初期化
     slack_client = WebClient(token=mycredential.SLACK_TOKEN)
 
     # 送信するメッセージ
-    message = "Pythonのコードからメッセージを投稿しています"
+    message = """
+    Pythonのコードからメッセージを投稿しています
+    改行しています
+    """
+    # ヒアドキュメントのインデントを除外する
+    deleted_indent_message = textwrap.dedent(message)
     try:
         response = slack_client.chat_postMessage(
-            channel=mycredential.SLACK_CHANNEL_ID, text=message
+            channel=mycredential.SLACK_CHANNEL_ID, text=deleted_indent_message
         )
-        logger.info(f"Slackにメッセージが投稿されました\t{message}")
+        logger.info(f"Slackにメッセージが投稿されました\t{deleted_indent_message}")
         logger.info(response)
     except Exception as e:
         logger.error(f"Slackへのメッセージ投稿中にエラーが発生しました\t{e}")
-
-
-def add_one(number: int) -> int:
-    return number + 1
 
 
 if __name__ == "__main__":
