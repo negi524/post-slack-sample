8beda004d7bee8765d0d2e4ddfa4b067aee06232
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-09-30 15:30:55 +0900

    投稿のコードを実装

--- post_slack_sample/main.py
+++ post_slack_sample/main.py
@@ -1,6 +1,7 @@
 import application_setting.my_logger as mylogger
 from logging import Logger
 import application_setting.my_credential as mycredential
+from slack_sdk import WebClient
 
 # loggerを取得
 logger: Logger = mylogger.get_logger("main")
@@ -8,8 +9,19 @@ logger: Logger = mylogger.get_logger("main")
 
 def main():
     logger.info("hello, world.")
-    print(mycredential.SLACK_TOKEN)
-    print(mycredential.SLACK_CHANNEL_ID)
+    # Slack WebClientの初期化
+    slack_client = WebClient(token=mycredential.SLACK_TOKEN)
+
+    # 送信するメッセージ
+    message = "Pythonのコードからメッセージを投稿しています"
+    try:
+        response = slack_client.chat_postMessage(
+            channel=mycredential.SLACK_CHANNEL_ID, text=message
+        )
+        logger.info(f"Slackにメッセージが投稿されました\t{message}")
+        logger.info(response)
+    except Exception as e:
+        logger.error(f"Slackへのメッセージ投稿中にエラーが発生しました\t{e}")
 
 
 def add_one(number: int) -> int:
